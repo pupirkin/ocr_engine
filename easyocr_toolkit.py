e7649591b0fdcbfb885c5f276ee863680e6afcc6
pupirkin <80263224+pupirkin@users.noreply.github.com>
2025-04-22 16:32:06 +0500

    Update easyocr_toolkit.py

--- easyocr_toolkit.py
+++ easyocr_toolkit.py
@@ -1,5 +1,7 @@
 from superagi.tools.base_tool import BaseToolkit
 from superagi.tools.external_tools.ocr_engine.tool1 import EasyOCRTool  # Путь к инструменту
+from .tool1 import ScanDocumentTool  # Import the OCR tool you created (ScanDocumentTool)
+
 
 class EasyOCRToolkit(BaseToolkit):
     name: str = "EasyOCR Toolkit"
